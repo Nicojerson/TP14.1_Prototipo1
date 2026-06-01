6d554a5c85e6256cdb89034623c8e62da0f772c8
Nicojerson <nicojersonsky1234@gmail.com>
2026-06-01 10:20:15 -0300

    Avances Unity Clase 2

---
+++ Assets/Scripts/InteractionArea.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+{   
+    int contador = 0;
+    void OnTriggerEnter(Collider other)
+    {
+
+    if(other.CompareTag("Coleccionable")){
+        Destroy(other.gameObject);
+        contador = contador + 1;
+        Debug.Log("El contador es: " + contador);
+    }
+    }
+}
